c33967a2c8a5570fcd238cb5d8f47a675602bf41
congdunghzz <lebaoduy023502@gmail.com>
2024-03-19 14:14:16 +0700

    fix button add to cart

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -6,14 +6,14 @@ function Categories (){
 
             <div className=" container-fruild d-flex flex-wrap align-items-center justify-content-center  py-3 my-5 ">   
                         <ul className=" nav  mb-2 justify-content-center mb-md-0">
-                            <li className="mx-3"><a href="#" className="nav-link px-2">All Products</a></li>
-                            <li className="mx-3"><a href="#" className="nav-link px-2">Smart Phone</a></li>
-                            <li className="mx-3"><a href="#" className="nav-link px-2">Lap Top</a></li>
-                            <li className="mx-3"><a href="#" className="nav-link px-2">Tablet</a></li>
-                            <li className="mx-3"><a href="#" className="nav-link px-2">Smart Watch</a></li>
-                            <li className="mx-3"><a href="#" className="nav-link px-2">Smart Watch</a></li>
-                            <li className="mx-3"><a href="#" className="nav-link px-2">Smart Watch</a></li>
-                            <li className="mx-3"><a href="#" className="nav-link px-2">Smart Watch</a></li>
+                            <li className="mx-3 fw-bold"><a href="#" className="nav-link px-2 text-secondary">All Products</a></li>
+                            <li className="mx-3 fw-bold"><a href="#" className="nav-link px-2 text-secondary">Smart Phone</a></li>
+                            <li className="mx-3 fw-bold"><a href="#" className="nav-link px-2 text-secondary">Lap Top</a></li>
+                            <li className="mx-3 fw-bold"><a href="#" className="nav-link px-2 text-secondary">Tablet</a></li>
+                            <li className="mx-3 fw-bold"><a href="#" className="nav-link px-2 text-secondary">Smart Watch</a></li>
+                            <li className="mx-3 fw-bold"><a href="#" className="nav-link px-2 text-secondary">Smart Watch</a></li>
+                            <li className="mx-3 fw-bold"><a href="#" className="nav-link px-2 text-secondary">Smart Watch</a></li>
+                            <li className="mx-3 fw-bold"><a href="#" className="nav-link px-2 text-secondary">Smart Watch</a></li>
                         </ul>    
             </div>
         </div>
