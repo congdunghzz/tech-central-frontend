6f55c226db3caf941f4ee5d1922650ff2d705103
congdunghzz <lebaoduy023502@gmail.com>
2024-04-10 21:39:20 +0700

    add UnAuthorized Error Page and restrict pages to users

--- src/App.js
+++ src/App.js
@@ -11,21 +11,27 @@ import Login from './pages/Login';
 import Admin from './pages/Admin';
 import Register from './pages/Register';
 import UserProfile from './pages/UserProfile';
+import UnAuthorized from './pages/UnAuthorized';
 function App() {
+
+  const authToken = window.localStorage.getItem('authToken');
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
         <Route path="/product/:id" element={<SingleProduct />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
         <Route path="/product" element={<Product />} />
-        <Route path="/orders" element={<YourOrders />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/profile" element={<UserProfile />} />
+
+        
+        <Route path="/checkout" element={authToken ? (<Checkout />) : (< UnAuthorized/>)} />
+        <Route path="/orders" element={authToken? (<YourOrders />) : (< UnAuthorized/>)} />
+        <Route path="/profile" element={authToken ? (<UserProfile />) : (< UnAuthorized/>)} />
+        <Route path="/admin" element={authToken ? (<Admin /> ) : (< UnAuthorized/>)} />
       </Routes>
 
     </>
---
+++ src/pages/UnAuthorized/index.js
@@ -0,0 +1,14 @@
+
+import "../Cart/Cart.css"
+function UnAuthorized() {
+    return (
+        <div className="min-vh-75 d-flex justify-content-center align-items-center">
+            <h1 className="text-danger text-center"> You Are Not Permitted To Access This Page </h1>
+            <br/>
+           
+                
+        </div>
+    )
+}
+
+export default UnAuthorized;
